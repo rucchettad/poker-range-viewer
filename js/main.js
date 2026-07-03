c5bb4b857152a0fceb83c5be3bac8b678f513cea
rucchettad <rucchettad@gmail.com>
2026-07-03 20:38:50 +0200

    Fix cache-busting su import moduli JS (risolve mobile.js bloccato su versione vecchia)

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@
  */
 'use strict';
 
-import { initAuth, ripristinaSessione } from './auth.js';
-import { initApp }   from './app.js';
-import { initTools } from './tools.js';
+import { initAuth, ripristinaSessione } from './auth.js?v=20260703c';
+import { initApp }   from './app.js?v=20260703c';
+import { initTools } from './tools.js?v=20260703c';
 
 initAuth();
 initApp();
